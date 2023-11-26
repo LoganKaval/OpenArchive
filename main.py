7fb5ac9c7a0fca976aa42644c1e6cc6af320f50a
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2023-11-25 19:04:13 -0600

    main.py
    The OpenArchive file.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ cpuinfo = platform.uname()
 
 #All of the syntax for OpenArchive are right here!!!
 data_lib = {
-    'archive_age': 'Older Than 0 Days Old',
+    'archive_age': 'Older Than 1 Days Old',
     'archive_creator': 'Logan Kaval',
     'archive_purpose': 'To Help People Remember Stuff',
     'archive_hardware': cpuinfo,
@@ -31,6 +31,7 @@ data_lib = {
     'pla_print_temp': '190C to 220C',
     'tpu_print_temp': '210C to 230C',
     'water_comp': '2 Hydrogen and 1 Oxygen',
+    "archive_ver": 'Version: v1.7.6 Flying Lion',
     'help_syntax': ' '
 
 
@@ -43,3 +44,4 @@ print(data_lib[question])
 while True:
     question = input('What Do You Want To Know? Enter in the Syntax Here (Do "help_syntax" for the Syntax) -->')
     print(data_lib[question])
+
